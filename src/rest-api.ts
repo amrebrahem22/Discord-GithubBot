6c0ae99b123fad5f00a9b7433c28f216d84df297
unknown <amrebrahem226@gmail.com>
2022-03-29 04:52:56 +0200

    cleanup code

--- src/rest-api.ts
+++ src/rest-api.ts
@@ -1,59 +1,69 @@
-import express, {Request, Response} from 'express';
-import { Client, TextChannel, MessageEmbed  } from 'discord.js';
-import Issue from './models/Issue';
-
-const GithubWebHook = require('express-github-webhook');
-const webhookHandler = GithubWebHook({ path: '/webhook', secret: 'secret' });
+import express, { Request, Response } from "express";
+import { Client, TextChannel, MessageEmbed } from "discord.js";
+import Issue from "./models/Issue";
 
 export function createRestApi(client: Client) {
     const app = express();
     app.use(express.json());
 
-    app.use(webhookHandler);
+    app.post("/github", async (req: Request, res: Response) => {
+        const { title, html_url, user, body, comment } = req.body.issue;
 
-    app.post('/github', async (req: Request, res: Response) => {
-        const {title, html_url, user, body, comment} = req.body.issue;
-        
-        const guild = client.guilds.cache.get(process.env.GUILD_ID as string)
+        const guild = client.guilds.cache.get(process.env.GUILD_ID as string);
 
         if (guild) {
-            const channel = (guild.channels.cache.get("958056228343402516") as TextChannel );
+            const channel = guild.channels.cache.get(
+                "958056228343402516"
+            ) as TextChannel;
             // if comment
-            if(req.body.action === 'created') {
-                // get thread id, and send this message
-                Issue.findOne({title}, async (err: any, issue: { threadId: any; }) => {
-                    if (err) console.log(err);
-
-                    const thread = channel.threads.cache.find((i: { id: any }) => i.id === issue.threadId);
-
-                    if (thread) thread.send(`**User** <@${comment.user.login}>
-                    **Comment: ** ${comment.body}`);
-                    });
-                
-            } else {
+            if (req.body.action === "opened") {
                 // new issue
                 const issueEmbed = new MessageEmbed()
-                .setColor('#0099ff')
-                .setTitle(title)
-                .setURL(html_url)
-                .setAuthor({ name: user.login, iconURL: user.avatar_url, url: user.html_url })
-                .setDescription(body)
-                .setTimestamp();
+                    .setColor("#0099ff")
+                    .setTitle(title)
+                    .setURL(html_url)
+                    .setAuthor({
+                        name: user.login,
+                        iconURL: user.avatar_url,
+                        url: user.html_url,
+                    })
+                    .setDescription(body)
+                    .setTimestamp();
 
-                channel.send({ embeds: [issueEmbed]});
+                channel.send({ embeds: [issueEmbed] });
 
                 // create thread
                 const thread = await (channel as TextChannel).threads.create({
                     name: `Github Issue ${title}`,
-                    reason: `Support ticket for ${title}`
+                    reason: `Support ticket for ${title}`,
                 });
-                
+
                 // send thread message
                 thread.send(`**User** <@${user.login}>
                 **Issue: ** ${title}`);
 
-                await Issue.create({title, body, threadId: thread.id, user: user.login});
+                await Issue.create({
+                    title,
+                    body,
+                    threadId: thread.id,
+                    user: user.login,
+                });
+            } else {
+                // get thread id, and send this message
+                Issue.findOne(
+                    { title },
+                    async (err: any, issue: { threadId: any }) => {
+                        if (err) console.log(err);
 
+                        const thread = channel.threads.cache.find(
+                            (i: { id: any }) => i.id === issue.threadId
+                        );
+
+                        if (thread)
+                            thread.send(`**User** <@${comment.user.login}>
+                    **Comment: ** ${comment.body}`);
+                    }
+                );
             }
         }
         res.json({ ok: 1, data: req.body });
